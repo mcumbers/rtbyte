435f9e0c52afd1e15a0092ade321af00bdc0c807
Michael Cumbers <me@michaelcumbers.ca>
2023-10-07 15:39:39 -0700

    Check for null embeds

--- src/listeners/guild-info-logs/guildLogCreate.ts
+++ src/listeners/guild-info-logs/guildLogCreate.ts
@@ -6,9 +6,9 @@ import type { BaseGuildTextChannel } from 'discord.js';
 
 @ApplyOptions<ListenerOptions>({ event: 'guildLogCreate' })
 export class UserEvent extends Listener {
-	public run(logChannel: BaseGuildTextChannel | null, logEmbeds: GuildLogEmbed[]) {
+	public run(logChannel: BaseGuildTextChannel | null, logEmbeds: GuildLogEmbed[] | null) {
 		if (isNullish(logChannel)) return;
-		if (!logEmbeds.length) return;
+		if (!logEmbeds || !logEmbeds.length) return;
 
 		return logChannel.send({ embeds: logEmbeds });
 	}
