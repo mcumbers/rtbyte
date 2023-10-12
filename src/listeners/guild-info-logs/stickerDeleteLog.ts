b790da4cdb53912fa4178762b16456c62c59c1b1
Michael Cumbers <me@michaelcumbers.ca>
2023-10-12 14:22:06 -0700

    New Styles for Sticker C/D/U

--- src/listeners/guild-info-logs/stickerDeleteLog.ts
+++ src/listeners/guild-info-logs/stickerDeleteLog.ts
@@ -2,38 +2,44 @@ import { GuildLogEmbed } from '#lib/extensions/GuildLogEmbed';
 import { getAuditLogExecutor } from '#utils/util';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, type ListenerOptions } from '@sapphire/framework';
-import { inlineCodeBlock, isNullish } from '@sapphire/utilities';
+import { isNullish } from '@sapphire/utilities';
 import { AuditLogEvent, BaseGuildTextChannel, Guild, Sticker, StickerFormatType, User } from 'discord.js';
 
 @ApplyOptions<ListenerOptions>({ event: Events.GuildStickerDelete })
 export class UserEvent extends Listener {
 	public async run(sticker: Sticker) {
 		if (isNullish(sticker.id)) return;
+		if (isNullish(sticker.guildId)) return;
 
-		const guildSettingsInfoLogs = await this.container.prisma.guildSettingsInfoLogs.findUnique({ where: { id: sticker.guild?.id } });
+		const guildSettingsInfoLogs = await this.container.prisma.guildSettingsInfoLogs.findUnique({ where: { id: sticker.guildId } });
 		if (!guildSettingsInfoLogs?.stickerDeleteLog || !guildSettingsInfoLogs.infoLogChannel) return;
 
 		const logChannel = sticker.guild?.channels.resolve(guildSettingsInfoLogs.infoLogChannel) as BaseGuildTextChannel;
-		const executor = await getAuditLogExecutor(AuditLogEvent.EmojiCreate, sticker.guild as Guild);
+		const executor = await getAuditLogExecutor(AuditLogEvent.StickerDelete, sticker.guild as Guild, sticker);
 
-		return this.container.client.emit('guildLogCreate', logChannel, this.generateGuildLog(sticker, executor));
+		return this.container.client.emit('guildLogCreate', logChannel, await this.generateGuildLog(sticker, executor));
 	}
 
-	private generateGuildLog(sticker: Sticker, executor: User | null | undefined) {
+	private async generateGuildLog(sticker: Sticker, executor: User | null) {
 		const embed = new GuildLogEmbed()
-			.setAuthor({
-				name: sticker.name,
-				url: sticker.url,
-				iconURL: sticker.url
-			})
-			.setDescription(inlineCodeBlock(sticker.id))
+			.setTitle('Sticker Deleted')
+			.setDescription(sticker.name)
 			.setThumbnail(sticker.url)
-			.addFields({ name: 'Format', value: inlineCodeBlock(StickerFormatType[sticker.format]), inline: true })
-			.setFooter({ text: `Sticker deleted ${isNullish(executor) ? '' : `by ${executor.username}`}`, iconURL: isNullish(executor) ? undefined : executor?.displayAvatarURL() })
+			.setFooter({ text: `Sticker ID: ${sticker.id}` })
 			.setType(Events.GuildStickerDelete);
 
-		if (sticker.tags) embed.addFields({ name: 'Emoji', value: `:${sticker.tags}:`, inline: true });
-		if (sticker?.createdTimestamp) embed.addFields({ name: 'Created', value: `<t:${Math.round(sticker.createdTimestamp as number / 1000)}:R>`, inline: true });
+		if (sticker.description) embed.addFields({ name: 'Description', value: sticker.description, inline: false });
+
+		embed.addFields({ name: 'Format', value: StickerFormatType[sticker.format], inline: true });
+
+		if (sticker.createdTimestamp) embed.addFields({ name: 'Created', value: `<t:${Math.round(sticker.createdTimestamp as number / 1000)}:R>`, inline: true });
+
+		if (sticker.tags) {
+			const emoji = await sticker.guild?.emojis.fetch(sticker.tags);
+			if (emoji) embed.addFields({ name: 'Emoji', value: `${emoji.toString()}`, inline: true });
+		}
+
+		if (!isNullish(executor)) embed.addFields({ name: 'Deleted By', value: executor.toString(), inline: false });
 
 		return [embed]
 	}
--- src/listeners/guild-info-logs/stickerUpdateLog.ts
+++ src/listeners/guild-info-logs/stickerUpdateLog.ts
@@ -1,42 +1,46 @@
 import { GuildLogEmbed } from '#lib/extensions/GuildLogEmbed';
-import { Emojis } from '#utils/constants';
 import { getAuditLogExecutor } from '#utils/util';
+import { UpdateLogStyle } from '@prisma/client';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, type ListenerOptions } from '@sapphire/framework';
-import { inlineCodeBlock, isNullish } from '@sapphire/utilities';
+import { isNullish } from '@sapphire/utilities';
 import { AuditLogEvent, BaseGuildTextChannel, Guild, Sticker, User } from 'discord.js';
 
 @ApplyOptions<ListenerOptions>({ event: Events.GuildStickerUpdate })
 export class UserEvent extends Listener {
 	public async run(oldSticker: Sticker, sticker: Sticker) {
 		if (isNullish(sticker.id)) return;
+		if (isNullish(sticker.guildId)) return;
 
-		const guildSettingsInfoLogs = await this.container.prisma.guildSettingsInfoLogs.findUnique({ where: { id: sticker.guild?.id } });
+		const guildSettingsInfoLogs = await this.container.prisma.guildSettingsInfoLogs.findUnique({ where: { id: sticker.guildId } });
 		if (!guildSettingsInfoLogs?.stickerUpdateLog || !guildSettingsInfoLogs.infoLogChannel) return;
 
 		const logChannel = sticker.guild?.channels.resolve(guildSettingsInfoLogs.infoLogChannel) as BaseGuildTextChannel;
-		const executor = await getAuditLogExecutor(AuditLogEvent.EmojiCreate, sticker.guild as Guild);
+		const executor = await getAuditLogExecutor(AuditLogEvent.StickerUpdate, sticker.guild as Guild, sticker);
 
-		return this.container.client.emit('guildLogCreate', logChannel, this.generateGuildLog(oldSticker, sticker, executor));
+		return this.container.client.emit('guildLogCreate', logChannel, await this.generateGuildLog(oldSticker, sticker, executor, guildSettingsInfoLogs.stickerUpdateLogStyle));
 	}
 
-	private generateGuildLog(oldSticker: Sticker, sticker: Sticker, executor: User | null | undefined) {
+	private async generateGuildLog(oldSticker: Sticker, sticker: Sticker, executor: User | null, style: UpdateLogStyle) {
 		const embed = new GuildLogEmbed()
-			.setAuthor({
-				name: sticker.name,
-				url: sticker.url,
-				iconURL: sticker.url
-			})
-			.setDescription(inlineCodeBlock(sticker.id))
+			.setTitle('Sticker Edited')
+			.setDescription(sticker.name)
 			.setThumbnail(sticker.url)
-			.setFooter({ text: `Sticker updated ${isNullish(executor) ? '' : `by ${executor.username}`}`, iconURL: isNullish(executor) ? undefined : executor?.displayAvatarURL() })
+			.setFooter({ text: `Sticker ID: ${sticker.id}` })
 			.setType(Events.GuildStickerUpdate);
 
-		const changes = [];
-		if (oldSticker.name !== sticker.name) changes.push(`${Emojis.Bullet}**Name changed**: ${inlineCodeBlock(`${oldSticker.name}`)} to ${inlineCodeBlock(`${sticker.name}`)}`);
-		if (oldSticker.tags !== sticker.tags) changes.push(`${Emojis.Bullet}**Emoji changed**: :${oldSticker.tags}: to :${sticker.tags}:`);
-		if (oldSticker.description !== sticker.description) changes.push(`${Emojis.Bullet}**Description changed**: ${inlineCodeBlock(`${oldSticker.description || 'Not set'}`)} to ${inlineCodeBlock(`${sticker.description || 'Not set'}`)}`);
-		if (changes.length) embed.addFields({ name: 'Changes', value: changes.join('\n') });
+
+		if (sticker.name !== oldSticker.name) embed.addDiffFields(oldSticker.name as string, sticker.name as string, 'Name', style);
+
+		if (sticker.description !== oldSticker.description) embed.addDiffFields(oldSticker.description as string, sticker.description as string, 'Description', style);
+
+		if (sticker.tags !== oldSticker.tags) {
+			const emoji = sticker.tags ? await sticker.guild?.emojis.fetch(sticker.tags) : null;
+			const oldEmoji = oldSticker.tags ? await sticker.guild?.emojis.fetch(oldSticker.tags) : null;
+			embed.addFields({ name: 'Emoji Changed', value: `${oldEmoji?.toString()}   ->   ${emoji?.toString()}`, inline: true });
+		}
+
+		if (!isNullish(executor)) embed.addFields({ name: 'Edited By', value: executor.toString(), inline: false });
 
 		return [embed]
 	}
