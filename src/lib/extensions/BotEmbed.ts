81b52d1fb93c0a8400188404df54536e096390a8
Michael Cumbers <me@michaelcumbers.ca>
2023-10-12 14:19:27 -0700

    addDiffFields

--- src/lib/extensions/BotEmbed.ts
+++ src/lib/extensions/BotEmbed.ts
@@ -1,4 +1,6 @@
 import { Colors, ZeroWidthSpace } from "#utils/constants";
+import { UpdateLogStyle } from "@prisma/client";
+import * as Diff from 'diff';
 import { EmbedBuilder, type APIEmbedField } from "discord.js";
 
 export class BotEmbed extends EmbedBuilder {
@@ -16,11 +18,58 @@ export class BotEmbed extends EmbedBuilder {
 				this.addBlankFields(field);
 			}
 		} else {
-			const fieldName: string = fields.name.length ? fields.name : ZeroWidthSpace;
-			const fieldValue: string = fields.value.length ? fields.value : ZeroWidthSpace;
-			const fieldInline: boolean = fields.inline ?? false;
-			return this.addFields({ name: fieldName, value: fieldValue, inline: fieldInline });
+			return this.addFields({ name: fields.name || ZeroWidthSpace, value: fields.value || ZeroWidthSpace, inline: fields.inline || false });
 		}
+		return this;
+	}
+
+	public addDiffFields(oldString?: string, newString?: string, descriptor?: string, style: UpdateLogStyle = UpdateLogStyle.before_after) {
+		if (oldString === newString) return this;
+
+		let inline = false;
+
+		if (!oldString) {
+			inline = !(newString && newString.length > 24);
+			this.addBlankFields({ name: `${descriptor} Added`, value: newString as string, inline });
+		}
+
+		if (!newString) {
+			inline = !(oldString && oldString.length > 24);
+			this.addBlankFields({ name: `${descriptor} Removed`, value: oldString as string, inline });
+		}
+
+		if (newString && oldString) {
+			inline = !(oldString.length > 24 || newString.length > 24);
+
+			if (style === UpdateLogStyle.after_only) {
+				this.addBlankFields({ name: `New ${descriptor}`, value: newString, inline });
+			}
+
+			if (style === UpdateLogStyle.before_after) {
+				this.addBlankFields({ name: `Old ${descriptor}`, value: oldString, inline });
+				this.addBlankFields({ name: `New ${descriptor}`, value: newString, inline });
+			}
+
+			if (style === UpdateLogStyle.before_only) {
+				this.addBlankFields({ name: `Old ${descriptor}`, value: oldString, inline });
+			}
+
+			if (style === UpdateLogStyle.diff) {
+				const diff = Diff.diffChars(oldString as string, newString as string);
+
+				let workingString = ``;
+
+				for (const part of diff) {
+					workingString += `${part.added ? `+` : part.removed ? `-` : `~`} ${part.value}\n`;
+				}
+
+				this.addBlankFields({ name: `${descriptor} Changes`, value: `\`\`\`diff\n${workingString.replaceAll('```', `\`${ZeroWidthSpace}\`\``)}\`\`\``, inline: false });
+				this.addBlankFields({ name: `New ${descriptor}`, value: newString || ``, inline: false });
+			}
+		}
+
+		if (inline) this.addBlankFields({ name: '', value: '', inline: true });
+
 		return this;
 	}
 }
