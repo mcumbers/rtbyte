138029d92bb2e3af6e212071d73abdd1111d22be
Michael Cumbers <me@michaelcumbers.ca>
2023-10-16 11:08:01 -0700

    Stage C/D Logs

--- src/listeners/guild-info-logs/stageInstanceCreateLog.ts
+++ src/listeners/guild-info-logs/stageInstanceCreateLog.ts
@@ -2,7 +2,7 @@ import { GuildLogEmbed } from '#lib/extensions/GuildLogEmbed';
 import { getAuditLogEntry } from '#utils/util';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, type ListenerOptions } from '@sapphire/framework';
-import { cutText, inlineCodeBlock, isNullish } from '@sapphire/utilities';
+import { isNullish } from '@sapphire/utilities';
 import { AuditLogEvent, BaseGuildTextChannel, Guild, StageInstance, type GuildAuditLogsEntry } from 'discord.js';
 
 @ApplyOptions<ListenerOptions>({ event: Events.StageInstanceCreate })
@@ -20,18 +20,16 @@ export class UserEvent extends Listener {
 	}
 
 	private generateGuildLog(stage: StageInstance, auditLogEntry: GuildAuditLogsEntry | null) {
+		if (!stage.guildId || !stage.channelId) return [];
+
 		const embed = new GuildLogEmbed()
-			.setAuthor({
-				name: cutText(stage.topic, 256),
-				url: `https://discord.com/channels/${stage.guildId}/${stage.channelId}`,
-				iconURL: stage.guild?.iconURL() ?? undefined
-			})
-			.setDescription(inlineCodeBlock(stage.id))
-			.addFields({ name: 'Stage channel', value: `<#${stage.channelId}>`, inline: true })
-			.setFooter({ text: `Stage started ${isNullish(auditLogEntry?.executor) ? '' : `by ${auditLogEntry?.executor.username}`}`, iconURL: isNullish(auditLogEntry?.executor) ? undefined : auditLogEntry?.executor?.displayAvatarURL() })
+			.setTitle('Stage Instance Started')
+			.setDescription(`${stage.channel?.toString()}${stage.topic ? `: ${stage.topic}` : ''}`)
+			.setThumbnail(stage.guild!.iconURL() as string)
+			.setFooter({ text: `Stage ID: ${stage.id}` })
 			.setType(Events.StageInstanceCreate);
 
-		if (stage.guildScheduledEvent) embed.addFields({ name: 'Associated event', value: `[${inlineCodeBlock(`${stage.guildScheduledEvent.name}`)}](${stage.guildScheduledEvent.url})`, inline: true });
+		if (stage.guildScheduledEvent) embed.addFields({ name: 'For Event', value: `[${stage.guildScheduledEvent.name}](${stage.guildScheduledEvent.url})`, inline: true });
 
 		if (auditLogEntry) {
 			if (!isNullish(auditLogEntry.reason)) embed.addFields({ name: 'Reason', value: auditLogEntry.reason, inline: false });
--- src/listeners/guild-info-logs/stageInstanceDeleteLog.ts
+++ src/listeners/guild-info-logs/stageInstanceDeleteLog.ts
@@ -2,7 +2,7 @@ import { GuildLogEmbed } from '#lib/extensions/GuildLogEmbed';
 import { getAuditLogEntry } from '#utils/util';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, type ListenerOptions } from '@sapphire/framework';
-import { cutText, inlineCodeBlock, isNullish } from '@sapphire/utilities';
+import { isNullish } from '@sapphire/utilities';
 import { AuditLogEvent, BaseGuildTextChannel, Guild, StageInstance, type GuildAuditLogsEntry } from 'discord.js';
 
 @ApplyOptions<ListenerOptions>({ event: Events.StageInstanceDelete })
@@ -20,19 +20,17 @@ export class UserEvent extends Listener {
 	}
 
 	private generateGuildLog(stage: StageInstance, auditLogEntry: GuildAuditLogsEntry | null) {
+		if (!stage.guildId || !stage.channelId) return [];
+
 		const embed = new GuildLogEmbed()
-			.setAuthor({
-				name: cutText(stage.topic, 256),
-				url: `https://discord.com/channels/${stage.guildId}/${stage.channelId}`,
-				iconURL: stage.guild?.iconURL() ?? undefined
-			})
-			.setDescription(inlineCodeBlock(stage.id))
-			.addFields({ name: 'Stage channel', value: `<#${stage.channelId}>`, inline: true })
+			.setTitle('Stage Instance Ended')
+			.setDescription(`${stage.channel?.toString()}${stage.topic ? `: ${stage.topic}` : ''}`)
 			.addFields({ name: 'Started', value: `<t:${Math.round(stage.createdTimestamp as number / 1000)}:R>`, inline: true })
-			.setFooter({ text: `Stage ended ${isNullish(auditLogEntry?.executor) ? '' : `by ${auditLogEntry?.executor.username}`}`, iconURL: isNullish(auditLogEntry?.executor) ? undefined : auditLogEntry?.executor?.displayAvatarURL() })
+			.setThumbnail(stage.guild!.iconURL() as string)
+			.setFooter({ text: `Stage ID: ${stage.id}` })
 			.setType(Events.StageInstanceDelete);
 
-		if (stage.guildScheduledEvent) embed.addFields({ name: 'Associated event', value: `[${inlineCodeBlock(`${stage.guildScheduledEvent.name}`)}](${stage.guildScheduledEvent.url})` });
+		if (stage.guildScheduledEvent) embed.addFields({ name: 'For Event', value: `[${stage.guildScheduledEvent.name}](${stage.guildScheduledEvent.url})`, inline: true });
 
 		if (auditLogEntry) {
 			if (!isNullish(auditLogEntry.reason)) embed.addFields({ name: 'Reason', value: auditLogEntry.reason, inline: false });
