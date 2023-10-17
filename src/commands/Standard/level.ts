9bc240413d051a88910c0174de33773adb6bae97
Michael Cumbers <me@michaelcumbers.ca>
2023-10-16 20:35:39 -0700

    Update XP function, dead simple XP command

---
+++ src/commands/Standard/level.ts
@@ -0,0 +1,35 @@
+import { getLevel } from '#utils/functions/xp';
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+
+@ApplyOptions<Command.Options>({
+	description: 'Check Your XP Level'
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+		);
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		await interaction.deferReply({ fetchReply: true, ephemeral: true });
+		const { prisma } = this.container;
+
+		// See if guild has XP disabled
+		if (!interaction.guild) return;
+		const guildSettingsXP = await prisma.guildSettingsXP.findFirst({ where: { id: interaction.guild.id } });
+		if (!guildSettingsXP || !guildSettingsXP.enabled) return;
+
+		// Create a memberDataXP record for the member if one doesn't exist
+		let memberDataXP = await prisma.memberDataXP.findFirst({ where: { userID: interaction.user.id, guildID: interaction.guild.id } });
+		if (!memberDataXP) memberDataXP = await prisma.memberDataXP.create({ data: { userID: interaction.user.id, guildID: interaction.guild.id } });
+
+		// Calculate the level info
+		const xpLevel = getLevel(memberDataXP.currentXP);
+		// Send message
+		return interaction.followUp({ content: `Level ${xpLevel.level} | ${xpLevel.levelXP}/${xpLevel.levelThreshhold}xp`, ephemeral: true });
+	}
+}
--- src/lib/util/functions/xp.ts
+++ src/lib/util/functions/xp.ts
@@ -2,14 +2,31 @@ const XP_MAX_LEVEL = 100;
 const XP_MESSAGE_MIN = 10;
 const XP_MESSAGE_MAX = 15;
 
-export function getLevel(xp: number) {
-	for (let lvl = 0, lvlXP = 0; lvl < XP_MAX_LEVEL; lvl++) {
-		const lvlup = (5 * (lvl ** 2) + (50 * lvl) + 100);
-		if (xp < (lvlXP += lvlup)) return lvl;
+export interface XPLevel {
+	level: number,
+	levelThreshhold: bigint,
+	levelXP: bigint,
+	totalXP: bigint
+}
+
+export function getLevel(totalXP: number | bigint) {
+	for (let level = 0, reqXP = 0n; level < XP_MAX_LEVEL; level++) {
+		const levelThreshhold = BigInt(5 * (level ** 2) + (50 * level) + 100);
+		if (totalXP < (reqXP += levelThreshhold)) return {
+			level,
+			levelThreshhold,
+			levelXP: BigInt(totalXP) - (reqXP - levelThreshhold),
+			totalXP
+		} as XPLevel;
 	}
-	return XP_MAX_LEVEL;
+	return {
+		level: XP_MAX_LEVEL,
+		levelThreshhold: 0n,
+		levelXP: 0n,
+		totalXP
+	} as XPLevel;
 }
 
 export function messageXPRoll(bonus: number = 0, multiplier: number = 1) {
-	return Math.floor((((Math.random() * XP_MESSAGE_MIN) + XP_MESSAGE_MAX) * multiplier) + bonus);
+	return BigInt(Math.floor((((Math.random() * XP_MESSAGE_MIN) + XP_MESSAGE_MAX) * multiplier) + bonus));
 }
