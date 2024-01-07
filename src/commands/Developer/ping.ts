bac51fe683223ab17737d31f6c282d2ad950f58d
Michael Cumbers <me@michaelcumbers.ca>
2024-01-07 13:46:03 -0800

    Update Ping--ad db ping

--- src/commands/Developer/ping.ts
+++ src/commands/Developer/ping.ts
@@ -1,11 +1,11 @@
 import { BotCommand } from '#lib/extensions/BotCommand';
-import { Emojis } from '#utils/constants';
+import { Prisma } from '@prisma/client';
 import { ApplyOptions } from '@sapphire/decorators';
 import { isMessageInstance } from '@sapphire/discord.js-utilities';
 import { type ChatInputCommand } from '@sapphire/framework';
 
 @ApplyOptions<ChatInputCommand.Options>({
-	description: 'Pings bot',
+	description: 'Check Bot\'s Ping',
 	preconditions: ['OwnerOnly']
 })
 export class UserCommand extends BotCommand {
@@ -17,11 +17,16 @@ export class UserCommand extends BotCommand {
 		const msg = await interaction.deferReply({ ephemeral: true, fetchReply: true });
 
 		if (isMessageInstance(msg)) {
-			const diff = msg.createdTimestamp - interaction.createdTimestamp;
-			const ping = Math.round(this.container.client.ws.ping);
-			return interaction.editReply(`${Emojis.Check} Pong! \`Bot: ${diff}ms\` \`API: ${ping}ms\``);
+			const ping = msg.createdTimestamp - interaction.createdTimestamp;
+			const dbPing = await this.getDBPing(Date.now());
+			return interaction.editReply(`🏓 Pong! \`Bot: ${ping}ms\` \`DB: ${dbPing}ms\``);
 		}
 
-		return interaction.editReply(`${Emojis.X} Failed to retrieve ping.`);
+		return interaction.editReply(`Failed to retrieve ping.`);
+	}
+
+	async getDBPing(startTime = Date.now()) {
+		const result = await this.container.prisma.$queryRaw(Prisma.sql`SELECT 1`);
+		return result ? Date.now() - startTime : null;
 	}
 }
