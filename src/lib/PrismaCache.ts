01902bf8b05d076a9303858a73e69c21f34ad9eb
Michael Cumbers <me@michaelcumbers.ca>
2024-02-20 11:55:19 -0800

    Split fetch() and fetchTuple() into fetchMany() and fetchTupleMany()

--- src/lib/PrismaCache.ts
+++ src/lib/PrismaCache.ts
@@ -32,7 +32,7 @@ export class PrismaCache<PrismaModel> {
 
 	public async create(args: Args<PrismaModel, 'create'>) {
 		const result = await this.prismaModel.create(args);
-		if (!result) return result;
+		if (!result) return result as null;
 		this._cache.set(result.id as string, result as PrismaModel);
 
 		return result as PrismaModel;
@@ -40,7 +40,7 @@ export class PrismaCache<PrismaModel> {
 
 	public async findUnique(args: Args<PrismaModel, 'findUnique'>) {
 		const result = await this.prismaModel.findUnique(args);
-		if (!result) return result;
+		if (!result) return result as null;
 		this._cache.set(result.id as string, result as PrismaModel);
 
 		return result as PrismaModel;
@@ -48,7 +48,7 @@ export class PrismaCache<PrismaModel> {
 
 	public async findMany(args: Args<PrismaModel, 'findMany'>) {
 		const result = await this.prismaModel.findMany(args);
-		if (!result || result.length) return result;
+		if (!result || !result.length) return result as null | PrismaModel[];
 		for (const entry of result) {
 			this._cache.set(entry.id as string, entry as PrismaModel);
 		}
@@ -57,7 +57,7 @@ export class PrismaCache<PrismaModel> {
 
 	public async update(args: Args<PrismaModel, 'update'>) {
 		const result = await this.prismaModel.update(args);
-		if (!result) return result;
+		if (!result) return result as null;
 		this._cache.set(result.id as string, result as PrismaModel);
 
 		return result as PrismaModel;
@@ -65,52 +65,62 @@ export class PrismaCache<PrismaModel> {
 
 	public async delete(args: Args<PrismaModel, 'delete'>) {
 		const result = await this.prismaModel.delete(args);
-		if (!result) return result;
+		if (!result) return result as null;
 		this._cache.delete(result.id as string);
 
 		return result as PrismaModel;
 	}
 
-	public async fetch(id: string | string[], force?: boolean) {
-		if (!id || !id.length) return undefined;
-		if (force) return this.findMany({ where: { id: Array.isArray(id) ? { in: id } : id } } as Args<PrismaModel, 'findMany'>);
+	public async fetch(id: string, force?: boolean) {
+		if (force) return this.findUnique({ where: { id } } as Args<PrismaModel, 'findUnique'>);
 
-		if (Array.isArray(id)) {
-			const unCached: string[] = id.filter((entry) => !this._cache.has(entry));
-			if (unCached.length) await this.findMany({ where: { id: { in: unCached } } } as Args<PrismaModel, 'findMany'>);
-			return Array.from(this._cache.filter((entry, key) => entry && id.includes(key)));
-		}
-
-		if (this._cache.has(id)) return this._cache.get(id);
+		if (this._cache.has(id)) return this._cache.get(id) as PrismaModel;
 
 		return this.findUnique({ where: { id } } as Args<PrismaModel, 'findUnique'>);
 	}
 
-	public async fetchTuple(ids: PrismaCacheIDTuple | PrismaCacheIDTuple[], keys: PrismaCacheIDTuple, force?: boolean) {
-		if (!ids.length) return undefined;
+	public async fetchMany(ids: string[], force?: boolean) {
+		if (!ids.length) return null;
+		if (force) return this.findMany({ where: { id: { in: ids } } } as Args<PrismaModel, 'findMany'>);
+
+		const unCached: string[] = ids.filter((entry) => !this._cache.has(entry));
+
+		if (unCached.length) await this.findMany({ where: { id: { in: unCached } } } as Args<PrismaModel, 'findMany'>);
+
+		return Array.from(this._cache.filter((entry, key) => entry && ids.includes(key))) as PrismaModel[];
+	}
+
+	public async fetchTuple(idPair: PrismaCacheIDTuple, keys: [string, string], force?: boolean) {
+		if (!idPair.length) return null;
 
 		const firstKey = <keyof PrismaModel>keys[0];
 		const secondKey = <keyof PrismaModel>keys[1];
 
-		if (Array.isArray(ids[0])) {
-			const idsArr = <Array<PrismaCacheIDTuple>>ids;
-			const unCached: any[] = [];
+		if (force) return this.findUnique({ where: { [`${firstKey as string}_${secondKey as string}`]: { [`${firstKey as string}`]: idPair[0], [`${secondKey as string}`]: idPair[1] } } } as Args<PrismaModel, 'findUnique'>);
 
-			for (const pair of idsArr) {
-				if (force || !this._cache.find((cached) => cached[firstKey] === pair[0] && cached[secondKey] === pair[1])) {
-					unCached.push({ [`${firstKey as string}`]: pair[0], [`${secondKey as string}`]: pair[1] });
-				}
+		const cached = this._cache.find((entry) => entry[firstKey] === idPair[0] && entry[secondKey] === idPair[1]);
+		if (cached) return cached as PrismaModel;
+
+		return this.findUnique({ where: { [`${firstKey as string}_${secondKey as string}`]: { [`${firstKey as string}`]: idPair[0], [`${secondKey as string}`]: idPair[1] } } } as Args<PrismaModel, 'findUnique'>);
+	}
+
+	public async fetchTupleMany(idPairs: PrismaCacheIDTuple[], keys: [string, string], force?: boolean) {
+		if (!idPairs.length) return null;
+
+		const firstKey = <keyof PrismaModel>keys[0];
+		const secondKey = <keyof PrismaModel>keys[1];
+
+		const unCached: any[] = [];
+
+		for (const idPair of idPairs) {
+			if (force || !this._cache.find((entry) => entry[firstKey] === idPair[0] && entry[secondKey] === idPair[1])) {
+				unCached.push({ [`${firstKey as string}`]: idPair[0], [`${secondKey as string}`]: idPair[1] });
 			}
-
-			if (unCached.length) await this.findMany({ where: { [`${firstKey as string}_${secondKey as string}`]: { OR: unCached } } } as Args<PrismaModel, 'findMany'>);
-
-			return Array.from(this._cache.filter((cached) => idsArr.includes([cached[firstKey], cached[secondKey]] as PrismaCacheIDTuple)).values());
 		}
 
-		const cached = this._cache.find((cached) => cached[firstKey] === ids[0] && cached[secondKey] === ids[1]);
+		if (unCached.length) await this.findMany({ where: { [`${firstKey as string}_${secondKey as string}`]: { OR: unCached } } } as Args<PrismaModel, 'findMany'>);
 
-		if (!force && cached) return cached;
+		return Array.from(this._cache.filter((cached) => idPairs.includes([cached[firstKey], cached[secondKey]] as PrismaCacheIDTuple)).values()) as PrismaModel[];
 
-		return this.findUnique({ where: { [`${firstKey as string}_${secondKey as string}`]: { [`${firstKey as string}`]: ids[0], [`${secondKey as string}`]: ids[1] } } } as Args<PrismaModel, 'findUnique'>);
 	}
 }
