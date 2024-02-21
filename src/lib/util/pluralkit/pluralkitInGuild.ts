f6b6cd1213098fd1349bd0d88987b988c8fc027d
Michael Cumbers <me@michaelcumbers.ca>
2024-02-21 11:31:47 -0800

    Tidy up pluralkitInGuild

--- src/lib/util/pluralkit/pluralkitInGuild.ts
+++ src/lib/util/pluralkit/pluralkitInGuild.ts
@@ -2,11 +2,5 @@ import { PluralKitBotID } from "#utils/constants";
 import type { Guild } from "discord.js";
 
 export async function pluralkitInGuild(guild: Guild) {
-	try {
-		const member = await guild.members.fetch(PluralKitBotID);
-		if (member) return true;
-	} catch (err) {
-		return false;
-	}
-	return false;
+	return guild.members.fetch(PluralKitBotID).catch(() => undefined);
 }
