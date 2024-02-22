5e700f5802bbbd439e63f84a84e42320e408346d
Michael Cumbers <me@michaelcumbers.ca>
2024-02-22 10:07:14 -0800

    Fix Send Command not targetting all TextBasedChannel types

--- src/commands/Moderation/send.ts
+++ src/commands/Moderation/send.ts
@@ -2,7 +2,7 @@ import { BotCommand } from '#lib/extensions/BotCommand';
 import type { CommandRunEvent } from '#root/listeners/control-guild-logs/commandRun';
 import { ApplyOptions } from '@sapphire/decorators';
 import { type ChatInputCommand } from '@sapphire/framework';
-import { ChannelType, PermissionFlagsBits, type GuildMember } from 'discord.js';
+import { PermissionFlagsBits, type GuildMember } from 'discord.js';
 
 @ApplyOptions<ChatInputCommand.Options>({
 	description: 'Sends a message to the specified channel as the bot',
@@ -38,7 +38,15 @@ export class UserCommand extends BotCommand {
 		const targetChannel = interaction.guild?.channels.resolve(interaction.options.getChannel('channel')?.id as string);
 		const messageInput = interaction.options.getString('message') as string;
 
-		if (targetChannel?.type !== ChannelType.GuildText) return interaction.followUp({ content: `Messages cannot be sent to ${targetChannel?.url}` });
+		if (!targetChannel) {
+			message = await interaction.followUp({ content: `Whoops! Something went wrong...` });
+			return this.container.client.emit('commandRun', { interaction, message, runtime: Date.now() - startTime, failed: true } as CommandRunEvent);
+		}
+
+		if (!targetChannel?.isTextBased()) {
+			message = await interaction.followUp({ content: `Messages cannot be sent to ${targetChannel?.url}` });
+			return this.container.client.emit('commandRun', { interaction, message, runtime: Date.now() - startTime } as CommandRunEvent);
+		}
 
 		if (!targetChannel.permissionsFor(interaction.member as GuildMember).has(PermissionFlagsBits.SendMessages)) {
 			message = await interaction.followUp({ content: `You don't have permission to send messages in ${targetChannel.url}`, components: [], embeds: [] });
