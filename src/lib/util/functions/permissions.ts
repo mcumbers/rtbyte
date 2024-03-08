c1808d0a151ddad559c7bf5639a94f6087216530
Michael Cumbers <me@michaelcumbers.ca>
2024-03-07 21:44:41 -0800

    Improved perm checks

--- src/lib/util/functions/permissions.ts
+++ src/lib/util/functions/permissions.ts
@@ -1,28 +1,49 @@
 import { getPermissionString } from "#utils/util";
+import { container } from "@sapphire/framework";
 import { GuildMember, PermissionsBitField } from "discord.js";
 
-export function isModerator(member: GuildMember) {
-	return isGuildOwner(member) || checkModerator(member) || checkAdministrator(member);
-}
-
-export function isAdmin(member: GuildMember) {
-	return isGuildOwner(member) || checkAdministrator(member);
-}
 
 export function isGuildOwner(member: GuildMember) {
 	return member.id === member.guild.ownerId;
 }
 
+export async function isAdmin(member: GuildMember) {
+	return isGuildOwner(member) || hasAdminRole(member);
+}
+
+export async function isModerator(member: GuildMember) {
+	return isGuildOwner(member) || hasAdminRole(member) || hasModrole(member);
+}
+
+export async function hasModrole(member: GuildMember) {
+	const { prisma } = container;
+	const guildSettings = await prisma.guildSettings.fetch(member.guild.id);
+
+	let hasModRole = false;
+	if (guildSettings?.moderatorRoles) {
+		for await (const modRoleID of guildSettings?.moderatorRoles) {
+			if (member.roles.cache.has(modRoleID)) hasModRole = true;
+		}
+	}
+
+	return hasModRole;
+}
+
+export async function hasAdminRole(member: GuildMember) {
+	const { prisma } = container;
+	const guildSettings = await prisma.guildSettings.fetch(member.guild.id);
+
+	let hasAdminRole = false;
+	if (guildSettings?.adminRoles) {
+		for await (const adminRoleID of guildSettings?.adminRoles) {
+			if (member.roles.cache.has(adminRoleID)) hasAdminRole = true;
+		}
+	}
+	return hasAdminRole;
+}
+
 export function checkRoleHierarchy(member: GuildMember, executor: GuildMember) {
 	return member.roles.highest.position < executor.roles.highest.position;
-}
-
-function checkModerator(member: GuildMember) {
-	return member.permissions.has(PermissionsBitField.Flags.KickMembers);
-}
-
-function checkAdministrator(member: GuildMember) {
-	return member.permissions.has(PermissionsBitField.Flags.ManageGuild);
 }
 
 export function getPermissionDifference(oldPermissions: PermissionsBitField, permissions: PermissionsBitField) {
